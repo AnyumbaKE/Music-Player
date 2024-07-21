22a0e9a9d186bc218f19aa370e64d6c5036ecbbc
AnyumbaKE <esanyumba@gmail.com>
2024-07-21 18:09:49 +0300

    music/views update

--- cloudPlayer/music/views.py
+++ cloudPlayer/music/views.py
@@ -5,6 +5,13 @@ from .models import Album
 # Create your views here.
 
 class IndexView(generic.ListView):
+    template_name = "music/index.html"
+    context_object_name = "all_albums"
+    
+    def get_queryset(self):
+        return Album.objects.all()
+    
+    
 class DetailView(generic.DetailView):
     model = Album
     template_name = "music/detail.html"
