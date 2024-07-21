f5bff173b5104210d26c39f67879d33052530e42
AnyumbaKE <esanyumba@gmail.com>
2024-07-21 18:06:27 +0300

    music/views update

--- cloudPlayer/music/views.py
+++ cloudPlayer/music/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Album
 
 # Create your views here.
+
+class IndexView(generic.ListView):
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = "music/detail.html"
+    
+class AlbumCreate(generic.CreateView):
+    model = Album
+    fields = ["artist", "album_title", "genre", "album_logo"]
