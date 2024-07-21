c9772d428325a39afd4cf157b5111c9a24adf069
AnyumbaKE <esanyumba@gmail.com>
2024-07-21 18:24:58 +0300

    music/urls update

---
+++ cloudPlayer/music/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+
+from . import views
+
+app_name = 'music'
+
+urlpatterns = [
+    
+   #/music/
+   path(" ", views.IndexView.as_view(), name="index"),
+   
+   #/music/{album_id}/
+   path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+   
+   #/music/album/add
+   path("album/add/", views.AlbumCreate.as_view(), name="album-add"),
+]
